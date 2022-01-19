039039676c1f3f95644d24d86b555edb5be48f42
FMartinez2022 <97995180+FMartinez2022@users.noreply.github.com>
2022-01-18 19:38:21 -0500

    Add files via upload

---
+++ payrate.py
@@ -0,0 +1,5 @@
+#print("Week 2 In Class Assignment")
+hours = input("How many hours did you work?")
+rate = input("What rate were you working at?")
+total = int(hours) * int(rate)
+print("You made", total, "dollars")
